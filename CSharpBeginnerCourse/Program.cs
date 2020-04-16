26a055dc7036365c497fa9fef2183708eb6781ef
Zain Fareed <zainfareed093@gmail.com>
2020-04-16 15:45:45 +0500

    Completed Lesson 18

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -6,12 +6,10 @@ namespace CSharpBeginnerCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0},{1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0},{1}", int.MinValue, int.MaxValue);
-            Console.WriteLine("{0},{1}", long.MinValue, long.MaxValue);
-            Console.WriteLine("{0},{1}", char.MinValue, char.MaxValue);
-            Console.WriteLine("{0},{1}", float.MinValue, float.MaxValue);
-            Console.WriteLine("{0},{1}", double.MinValue, double.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+            Pi = 15.45;     //giving error
+
         }
     }
 }
