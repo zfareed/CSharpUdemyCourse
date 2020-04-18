ba67e959138b6c1ff4c5bde6636c8a3965600c5e
Zain Fareed <zainfareed093@gmail.com>
2020-04-18 12:15:01 +0500

    Completed Lesson 25 (Summary)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -8,18 +8,13 @@ namespace CSharpBeginnerCourse
         {
             //Lesson 23. Opreators
 
-            var a = 3;
-            var b = 5;
-            var c = 2;
 
-            Console.WriteLine(a > b);
-            Console.WriteLine(a < b && b < c);
-            Console.WriteLine(a > b || b > c);
-            Console.WriteLine(!(a != b));
-            Console.WriteLine( a == c);
-
-           
+            byte num1 = 255;
+            num1 += 255;
             
+            Console.WriteLine(num1);
+
+
         }
     }
 }
