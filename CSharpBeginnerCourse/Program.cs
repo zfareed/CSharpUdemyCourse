168017b3e07e5980e9990f951330eee8fa08f6cf
Zain Fareed <zainfareed093@gmail.com>
2020-04-19 10:42:01 +0500

    Started Lesson 35 (Demo Lessons)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -1,36 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSharpBeginnerCourse
 {
+
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3
+
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            string fNmae = "zain";
-            string lNmae = "fareed";
-
-            Console.WriteLine(fNmae);
-            Console.WriteLine(lNmae);
-            Console.WriteLine(fNmae + " " + lNmae);
-
-            string fullNmae = string.Format("My name is {0} {1}", fNmae, lNmae);
-            Console.WriteLine(fullNmae);
-
-            string myfullNmae = string.Format("My name is {1} {0}", fNmae, lNmae);
-            Console.WriteLine(myfullNmae);
-
-
-            string[] names = new string[3] { "zain", "ahmad", "akbar" };
-            string mynames = string.Join(',', names);
-            Console.WriteLine(mynames);
-
-            // Verbanium Strings
-            Console.WriteLine("Hi Ahmad look at these links\nc://documents//folder1\nc://music//folder2");
-            Console.WriteLine(@"Hi Ahmad look at these links
-c:/documents/folder1
-c:/music/folder2");
-
+            var method = ShippingMethod.RegularAirMail;
+            Console.WriteLine((int)method);
 
         }
     }
