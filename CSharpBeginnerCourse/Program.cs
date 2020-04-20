e2e531b6f1233b02d38de3275b19f96b5f5ccfe6
Zain Fareed <zainfareed093@gmail.com>
2020-04-20 20:40:17 +0500

    Lesson 43 Exercises (Q-2)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -12,22 +12,25 @@ namespace CSharpBeginnerCourse
         {
             //Exercises
 
-            /* Q-1 - Write a program and ask the user to enter a number. The number should be between
-             1 to 10.If the user enters a valid number, display "Valid" on the console.Otherwise,
-             display "Invalid". (This logic is used a lot in applications where values entered into
-                 input boxes need to be validated.)*/
+            /* Q-2 - Write a program which takes two numbers from the console and displays the maximum
+                of the two.*/
 
-            Console.WriteLine("Enter a number : ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number_1 :");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number_2 :");
+            int number2 = Convert.ToInt32(Console.ReadLine());
 
-            if (number >= 1 && number <=10)
+            int greater_number;
+            if (number1 > number2)
             {
-                Console.WriteLine("Number is Valid");
+                greater_number = number1;
             }
             else
             {
-                Console.WriteLine("Invalid Number");
+                greater_number = number2;
             }
+
+            Console.WriteLine("Greater number is {0}", greater_number);
 
 
 
