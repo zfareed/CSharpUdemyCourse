43dadb276e689dad8361174c290c26200d2b8633
Zain Fareed <zainfareed093@gmail.com>
2020-04-20 17:57:54 +0500

    Completed Lesson 38 (Demo. Reference Types and Value Types 2)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpBeginnerCourse
 {
+    public class Person
+    {
+        public int Age;
+    }
 
    
     class Program
     {
         static void Main(string[] args)
         {
-            var value1 = 10;
-            var value2 = value1;
-            value2++;
-            Console.WriteLine(string.Format("value1 : {0} , value2 : {1}", value1, value2));
 
+            var number = 1;
+            Increament(number);
+            Console.WriteLine(number);
 
-            int[] array1 = new int[3] { 1, 2, 3 };
-            int[] array2 = array1;
-            array2[0] = 5;
-            Console.WriteLine(string.Format("array1[0] : {0} , array2[1] : {1}", array1[0], array2[0]));
+            Person person = new Person() { Age = 10};
+            AgeOld(person);
+            Console.WriteLine(person.Age);
 
+          
+        }
+
+        public static void Increament(int number)
+        {
+            number += 10;
+        }
+
+        public static void AgeOld(Person person)
+        {
+            person.Age += 20;
         }
     }
 }
