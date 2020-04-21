f0156e3348a3cbb66c3b2fec34ab6ba81ef94bb7
Zain Fareed <zainfareed093@gmail.com>
2020-04-21 14:43:23 +0500

    Completed Lesson 45 (Demo. Looops)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -10,7 +10,20 @@ namespace CSharpBeginnerCourse
     {
         static void Main(string[] args)
         {
-            
+           
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number = i * 1;
+                Console.WriteLine(number);
+            }
+
+
+            for (int i = 10; i > 0; i--)
+            {
+                int number2 = i * 1;
+                Console.WriteLine(number2);
+            }
 
         }
 
