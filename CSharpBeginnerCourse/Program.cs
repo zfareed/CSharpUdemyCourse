0198451e4c24cc9b1e3993dedfbcd7d7d9650805
Zain Fareed <zainfareed093@gmail.com>
2020-04-22 17:24:26 +0500

    Completed Lesson 59 (DateTime)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -13,90 +13,22 @@ namespace CSharpBeginnerCourse
     {
         static void Main(string[] args)
         {
-            // 56. Exercises
+            // Date and Time
 
-            /*Q-1 - When you post a message on Facebook, depending on the number of people who like your post,
-             Facebook displays different information.
+            DateTime datetime = new DateTime();
 
-                 If no one likes your post, it doesn't display anything.
-                 If only one person likes your post, it displays: [Friend's Name] likes your post.
-                 If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
-                 If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number
-                     of Other People] others like your post.
-                 Write a program and continuously ask the user to enter different names, until the user
-                     presses Enter (without supplying a name). Depending on the number of names provided,
-                 display a message based on the above pattern.*/
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            Console.WriteLine(now);
+            Console.WriteLine(today);
 
 
 
-
-
-            /* Q-2 - Write a program and ask the user to enter their name. Use an array to reverse the
-                 name and then store the result in a new string.Display the reversed name on the console.*/
-
-
-            /*Console.Write("Enter the name : ");
-            var name = Console.ReadLine();
-
-            char[] array = new char[10];
-            int i = 0;
-            foreach (char character in name)
-            {
-                array[i] = character;
-                i++;
-            }
-
-            Array.Reverse(array);
-
-            string newName = new string(array);
-            Console.WriteLine("Reverse Name : " + newName);*/
-
-
-
-            /*Q-3 - Write a program and ask the user to enter 5 numbers.If a number has been previously
-                entered, display an error message and ask the user to re-try. Once the user successfully
-                enters 5 unique numbers, sort them and display the result on the console.*/
-
-
-
-
-
-
-            /* Q-4 - Write a program and ask the user to continuously enter a number or type "Quit" to exit.
-                 The list of numbers may include duplicates.Display the unique numbers that the user has entered.*/
-
-            /*
-                        var mylist = new List<int>();
-
-                        while (true)
-                        {
-                            Console.Write(" Enter the number :");
-                            var number = Console.ReadLine();
-
-                            if (number == "quit")
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                int intNumber = Convert.ToInt32(number);
-                                mylist.Add(intNumber);
-
-                            }
-                        }
-
-                        IEnumerable list = mylist.Distinct();
-                        foreach (var item in list)
-                        {
-                            Console.WriteLine(item);
-                        }*/
-
-
-            
-
-
-
-
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd"));
 
         }
 
