a8bbfda543b4e76e8576d0195a5d1e0d1a439114
Zain Fareed <zainfareed093@gmail.com>
2020-04-25 14:36:32 +0500

    Lesson 68 Exercises (Q-1)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -14,21 +14,39 @@ namespace CSharpBeginnerCourse
     {
         static void Main(string[] args)
         {
-            // Demo. SrringBuilder
 
-            //Best for string manipulation   
-            var string_builder = new StringBuilder();
-            string_builder.Append('-', 10);
-            string_builder.AppendLine();
-            string_builder.Append("Header");
-            string_builder.AppendLine();
-            string_builder.Append('-', 10);
-            string_builder.Replace('-','+');
-            string_builder.Remove(0,10);
-            string_builder.Insert(0,new string('-', 10));
-            Console.WriteLine(string_builder);
+            // Exercises
 
-            Console.WriteLine(string_builder[0]);
+            /* Q-1 - Write a program and ask the user to enter a few numbers separated by a hyphen.
+                 Work out if the numbers are consecutive.For example, if the input is "5-6-7-8-9" or
+                 "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".*/
+
+            Console.Write("Enter the numbers seprated by hypen : ");
+            var numbers = Console.ReadLine();
+            var list = new List<int>();
+
+            var splitNUmbers = numbers.Split("-");
+
+            foreach (var number in splitNUmbers)
+            {
+                int intNumber = Convert.ToInt32(number);
+                list.Add(intNumber);
+            }
+
+
+            var fIndex = list[0];
+            var lIndex = list[list.Count-1];
+            var length = list.Count;
+
+            if ((fIndex+(length-1)) == lIndex)
+            {
+                Console.WriteLine("Consectuive");
+            }
+            else
+            {
+                Console.WriteLine("Non-Consectuive");
+            }
+
 
 
         }
