dfe1847b9301b819d3e19569f6abcc9cbffd9c38
Zain Fareed <zainfareed093@gmail.com>
2020-04-27 12:44:46 +0500

    Completed Lesson 76 (Exercises)

--- CSharpBeginnerCourse/Program.cs
+++ CSharpBeginnerCourse/Program.cs
@@ -17,17 +17,47 @@ namespace CSharpBeginnerCourse
         static void Main(string[] args)
         {
 
-            // Demo. Path
-            var path = @"C:\Users\Zain Fareed\Documents\DOC\Semester 6\OS\Lectures\Lecture1.pdf";
+            // Exercises
 
-            Console.WriteLine("Extension : " + Path.GetExtension(path));
-            Console.WriteLine("File Name : " + Path.GetFileName(path));
-            Console.WriteLine("File Name without Extension : " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("Directory Name : " + Path.GetDirectoryName(path));
+            /* Q-1 - Write a program that reads a text file and displays the number of words.*/
 
-                
+            /*var text = File.ReadAllText(@"C:\Users\Zain Fareed\Documents\DOC\mytextfile.txt");
+            Console.WriteLine(text);
+
+            int numberOfWords = 0;
+            var words = text.Split(" ");
+            foreach (var word in words)
+            {
+                numberOfWords++;
+            }
+
+            Console.WriteLine("Number of words : " + numberOfWords);*/
 
 
+            /*Q-2 - Write a program that reads a text file and displays the longest word in the file.*/
+
+            var text = File.ReadAllText(@"C:\Users\Zain Fareed\Documents\DOC\mytextfile.txt");
+            Console.WriteLine(text);
+
+            var words = text.Split(" ");
+            var wordList = new int[130];
+            int i = 0;
+             foreach (var word in words)
+             {
+                 var wordLenght = word.Length;
+                 wordList[i] = wordLenght;
+                 i++;
+             }
+            int longestWord = wordList.Max();
+            int index = Array.IndexOf(wordList, longestWord);
+
+            Console.WriteLine();
+            Console.WriteLine("Longest Word : " + words[index]);
+
+
+
+
+            
         }
 
     }
