548882e3d4879786d0992bcb5a539e3358fe0688
Zain Fareed <zainfareed093@gmail.com>
2020-04-30 18:14:37 +0500

    Completed Lesson 7 (Classes)

--- CSharpIntermediateCourse/Program.cs
+++ CSharpIntermediateCourse/Program.cs
@@ -2,11 +2,31 @@
 
 namespace CSharpIntermediateCourse
 {
+
+
+    public class Person
+    {
+        public string Name;
+
+        public void Age(int age)
+        {
+            Console.WriteLine("Age of " + Name + " is " + age);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var person = new Person();
+            person.Name = "Zain";
+            int age = 21;
+            person.Age(age);
+
+
+
         }
     }
 }
