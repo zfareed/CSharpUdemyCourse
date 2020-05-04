e6c3dd5ebb2788ea7b2532647cfdd7ecd56ba78a
Zain Fareed <zainfareed093@gmail.com>
2020-05-04 14:42:02 +0500

    Completed Lesson 24 (Upcasting and Downcasting)

--- CSharpIntermediateCourse/Program.cs
+++ CSharpIntermediateCourse/Program.cs
@@ -31,14 +31,24 @@ namespace CSharpIntermediateCourse
     {
         static void Main(string[] args)
         {
-            var text = new Text();         //text and shape both are refering to same object in the memory
-            Shape shape = text;
+             var text = new Text();         //text and shape both are refering to same object in the memory
+             Shape shape = text;
 
             text.Width = 100;
-            //shape.Width = 200;
+            shape.Width = 200;
 
 
             System.Console.WriteLine(text.Width);
+
+
+            //Downcasting
+
+            Shape myshape = new Text();
+            Text mytext = (Text)myshape;
+            
+
+
+
 
         }
     }
