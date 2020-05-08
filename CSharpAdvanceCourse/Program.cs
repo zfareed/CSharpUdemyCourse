fe962164e42974dc7bc367f30e108ffe2569832d
Zain Fareed <zainfareed093@gmail.com>
2020-05-08 16:59:05 +0500

    Completed Section 7 (Delegates)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -2,6 +2,19 @@
 
 namespace CSharpAdvanceCourse
 {
+    public class PhotoProcessor
+    {
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+
+
     public class PhotoFilters
     {
         public void ApplyBrightness(Photo photo)
@@ -22,22 +35,39 @@ namespace CSharpAdvanceCourse
 
     public class Photo
     {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var photo = new PhotoFilters();
-            photo.ApplyBrightness(new Photo());
-            photo.ApplyBrightness(new Photo());
-            photo.Resize(new Photo());
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
 
+            processor.Process("photo.jpg", filterHandler);
+        }
 
-
-
-            
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
 
         }
+
+
+
+        
     }
 }
+
