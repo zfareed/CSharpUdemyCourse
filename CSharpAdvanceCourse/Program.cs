47c4e9d4056b041b8e19505fa6dd64cdb246e4e0
Zain Fareed <zainfareed093@gmail.com>
2020-05-08 21:44:41 +0500

    Lesson 9 (Events and Delegates)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -13,10 +13,22 @@ namespace CSharpAdvanceCourse
 
     public class VideoEncoder
     {
+        public delegate void VideoEncoderEventHandler(object source, EventArgs argd);
+        public event VideoEncoderEventHandler VideoEncoded;
+
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
         }
     }
 
@@ -28,12 +40,23 @@ namespace CSharpAdvanceCourse
         static void Main(string[] args)
         {
             var video = new Video() { Title = "video 1" };
-            var videoencoder = new VideoEncoder();
+            var videoencoder = new VideoEncoder();//publisher
+            var mailService = new MailService();//subscriber
+
+            videoencoder.VideoEncoded += mailService.OnVideoEncoded;
 
             videoencoder.Encode(video);
 
 
 
+        }
+
+        public class MailService
+        {
+            public void OnVideoEncoded(object source, EventArgs agrs)
+            {
+                Console.WriteLine("Mail Sevice Sending...");
+            }
         }
 
         
