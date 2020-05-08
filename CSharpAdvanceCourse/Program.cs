7946b3adb5294e7ad5503fdbaffd6901bcb28dbd
Zain Fareed <zainfareed093@gmail.com>
2020-05-08 16:05:52 +0500

    Started Lesson 6 (Generics)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -2,11 +2,57 @@
 
 namespace CSharpAdvanceCourse
 {
+    public class List
+    {
+        public void Add(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+    }
+    public class BooKList
+    {
+            public void Add(Book book)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int this[int index]
+            {
+                get { throw new NotImplementedException(); }
+            }
+    }
+
+    public class Book
+        {
+            /// mybook
+        }
+            
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var list = new List();
+            list.Add(10);
+
+
+            var books = new BooKList();
+            books.Add(new Book());
+
+
+            
+
         }
     }
 }
