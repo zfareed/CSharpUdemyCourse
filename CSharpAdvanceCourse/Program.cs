b006073a12b630b47fa8ba902aab1460f1a0f854
Zain Fareed <zainfareed093@gmail.com>
2020-05-08 20:26:52 +0500

    Started Secion 9 (Events)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace CSharpAdvanceCourse
 {
-    public class BookRepository
+
+    public class Video
     {
-        public List<Book> GetBooks()
+        public string Title { get; set; }
+    }
+
+
+    public class VideoEncoder
+    {
+        public void Encode(Video video)
         {
-            return new List<Book>
-            {
-                new Book() {Title = "Title 1", Price = 5},
-                new Book() {Title = "Title 2", Price = 7},
-                new Book() {Title = "Title 3", Price = 17}
-            };
+            Console.WriteLine("Encoding video...");
+            Thread.Sleep(3000);
         }
     }
 
-    public class Book
-    {
-        public string Isbn { get; set; }
-        public string Title { get; set; }
-        public float Price { get; set; }
-    }
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            var books = new BookRepository().GetBooks();
+            var video = new Video() { Title = "video 1" };
+            var videoencoder = new VideoEncoder();
 
-            var cheapBooks = books.FindAll(b => b.Price < 10);
+            videoencoder.Encode(video);
 
-            foreach (var book in cheapBooks)
-            {
-                Console.WriteLine(book.Title);
-            }
+
+
         }
 
         
