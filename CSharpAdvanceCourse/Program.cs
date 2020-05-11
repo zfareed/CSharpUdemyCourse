0a6463a365d15c725ebac88952b27ca4df5a36f3
Zain Fareed <zainfareed093@gmail.com>
2020-05-11 21:10:03 +0500

    Completed Lesson 14 (Exception Handling)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -5,21 +5,56 @@ using System.Threading;
 
 namespace CSharpAdvanceCourse
 {
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access YouTube web service 
+                // Read the data 
+                // Create a list of Video objects
+                throw new Exception("Oops some low level YouTube error.");
+            }
+            catch (Exception ex)
+            {
+                // Log 
+
+                throw new YouTubeException("Could not fetch the videos from YouTube.", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+
+    public class YouTubeException : Exception
+    {
+        public YouTubeException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+    public class Video
+    {
+
+    }
+
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            dynamic number = 10;
-
-            Console.WriteLine(number.GetType());
-
-            int i = 5;
-            dynamic d = i;
-            long l = d;
-
-            Console.WriteLine(l);
-
+            try
+            {
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("mosh");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
