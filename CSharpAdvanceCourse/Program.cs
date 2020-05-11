a8f7d8565b78c2db1acd9b8a1743587f61f34c97
Zain Fareed <zainfareed093@gmail.com>
2020-05-11 20:14:55 +0500

    Completed Lesson 12 (Nullable Types)

--- CSharpAdvanceCourse/Program.cs
+++ CSharpAdvanceCourse/Program.cs
@@ -6,50 +6,24 @@ using System.Threading;
 namespace CSharpAdvanceCourse
 {
 
-    public class Book
-    {
-        public string Title { get; set; }
-        public float Price { get; set; }
-    }
-
-    public class BookRepository
-    {
-        public IEnumerable<Book> GetBooks()
-        {
-            return new List<Book>
-            {
-                new Book() {Title = "ADO.Net Step by Step", Price = 5 },
-                new Book() {Title = "ASP.NET MVC", Price = 9.99f },
-                new Book() {Title = "ASP.NET Web API", Price = 12 },
-                new Book() {Title = "C# Advanced Topics", Price = 7 },
-                new Book() {Title = "C# Advanced Topics", Price = 9 }
-            };
-        }
-    }
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            var books = new BookRepository().GetBooks();
 
-            // LINQ Query Opreators
-            var book = books.Single(b => b.Title == "ASP.NET MVC");
-            var book1 = books.SingleOrDefault(b => b.Title == "ASP.NET MVC 2");
-            var book2 = books.First(b => b.Title == "C# Advanced Topics");
+            DateTime? date = null;
 
-            Console.WriteLine(book.Title);
-            Console.WriteLine(book1 == null);
-            Console.WriteLine(book2.Title +" " + book2.Price);
+            Console.WriteLine("Get value or default :" + date.GetValueOrDefault());
+            Console.WriteLine("Has Value :" + date.HasValue);
+            Console.WriteLine("Value " + date.Value);   //program crash becasue it doesnot have value
 
+            DateTime date2 = date.GetValueOrDefault();
 
-            var pagedBooks = books.Skip(2).Take(3);
+            Console.WriteLine(date2);
 
-            foreach (var pagebook in pagedBooks)
-            {
-                Console.WriteLine(pagebook.Title);
-            }
+            DateTime date3 = date2;
+
+            Console.WriteLine(date3);
         }
     }
     
